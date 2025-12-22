8d9802f8bb9b7d53a6c30b07ea5fb60abd511a4b
Kohyuckjoon <akhj123akhj@naver.com>
2025-12-22 18:19:02 +0900

    2025.12.22
    UI 수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.R.attr.navigationIcon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -31,8 +32,23 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CheckboxDefaults.colors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.test.junit4.createComposeRule
+import kotlin.io.path.Path
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -44,98 +60,170 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home( onClick: () -> Unit ) {
-    val lightBlue = Color(0xA6DAF4)
 
-    Column(
-        modifier = Modifier
-            .background(Color.White)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Card(
-            modifier = Modifier
-                .clip(RoundedCornerShape(15.dp))
-                .padding(20.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = Color(0xFFD1E4FF)),
-            onClick = { onClick() }
-        ) {
-            Column (
-                modifier = Modifier
-                    .clickable(onClick = onClick)
-                    .padding(32.dp)
-                    .fillMaxWidth()
-                    .background(Color(0xFFD1E4FF)),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text("총 자산 현황",
-                    fontSize = 15.sp,
-                    color = Color.Black)
+    Scaffold(
+        // 전체 구조
+        containerColor = Color.DarkGray,
 
-                Text("₩ 1,500,000",
-                    Modifier.padding(0.dp, 5.dp, 0.dp, 5.dp),
-                    fontSize = 35.sp,
-                    color = Color.Black)
+        topBar = {
+            // 상단 바
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
 
-                Text("전월 대비  + 1.5%",
-                    fontSize = 14.sp,
-                    color = Color.Green)
-            }
-        }
+                // 가운데서부터 시작
+                title = {
+                    Text("Finance Snapshot")
+                },
 
-        Column(
-            modifier = Modifier
-                .padding(20.dp, 20.dp, 5.dp, 5.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Top
-        ) {
-            Text("자산 분류 비율 (차트 대체)",
-                fontSize = 20.sp)
-        }
+                /**
+                 * Slot 종류
+                 * 오른쪽 : action / 왼쪽 : navigationIcon /가운데 : title
+                 */
+                // 오른쪽에서부터 시작
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "설정"
+                        )
+                    }
+                },
 
-        Card (
-            modifier = Modifier
-                .clickable(onClick = onClick)
-                .clip(RoundedCornerShape(15.dp))
-                .padding(20.dp, 10.dp, 20.dp, 5.dp)
-                .fillMaxWidth()
-        ) {
-            Column (modifier = Modifier
-                .padding(32.dp)
-                .fillMaxWidth(),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally){
-
-                Text("파이 차트 시각영역")
-            }
-        }
-
+                // 왼쪽에서부터 시작
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "뒤로가기"
+                        )
+                    }
+                },
+            )
+        },
+    ) { innerPadding ->
         Column (
             modifier = Modifier
-                .padding(20.dp, 20.dp, 5.dp, 5.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Top
-        ) {
-            Text("주요 자산 목록 / 최근 활동",
-                fontSize = 20.sp)
-        }
-
-        val assetList = listOf("은행 잔고 1", "은행 잔고 2", "은행 잔고 3")
-        LazyColumn (
-            modifier = Modifier
+                .padding(innerPadding)
                 .fillMaxSize()
+                .background(Color.White)
+                .verticalScroll(rememberScrollState())
         ) {
-            items(assetList) { asset ->
-                Text(text = asset, modifier = Modifier.padding(16.dp))
-                HorizontalDivider()
+            Card (
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xC9F7FF))
+            ) {
+                Column (
+                    modifier = Modifier
+                        .padding(32.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("총 자산 현황")
+                    Text("₩ 150,000")
+                    Text("전월 대비 + 1.5%")
+                }
             }
         }
     }
 }
+
+
+//    val lightBlue = Color(0xA6DAF4)
+//
+//    Column(
+//        modifier = Modifier
+//            .background(Color.White)
+//            .fillMaxSize(),
+//        verticalArrangement = Arrangement.Top,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        Card(
+//            modifier = Modifier
+//                .clip(RoundedCornerShape(15.dp))
+//                .padding(20.dp),
+//            colors = CardDefaults.cardColors(
+//                containerColor = Color(0xFFD1E4FF)),
+//            onClick = { onClick() }
+//        ) {
+//            Column (
+//                modifier = Modifier
+//                    .clickable(onClick = onClick)
+//                    .padding(32.dp)
+//                    .fillMaxWidth()
+//                    .background(Color(0xFFD1E4FF)),
+//                verticalArrangement = Arrangement.Top,
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//            ) {
+//                Text("총 자산 현황",
+//                    fontSize = 15.sp,
+//                    color = Color.Black)
+//
+//                Text("₩ 1,500,000",
+//                    Modifier.padding(0.dp, 5.dp, 0.dp, 5.dp),
+//                    fontSize = 35.sp,
+//                    color = Color.Black)
+//
+//                Text("전월 대비  + 1.5%",
+//                    fontSize = 14.sp,
+//                    color = Color.Green)
+//            }
+//        }
+//
+//        Column(
+//            modifier = Modifier
+//                .padding(20.dp, 20.dp, 5.dp, 5.dp)
+//                .fillMaxWidth(),
+//            verticalArrangement = Arrangement.Top
+//        ) {
+//            Text("자산 분류 비율 (차트 대체)",
+//                fontSize = 20.sp)
+//        }
+//
+//        Card (
+//            modifier = Modifier
+//                .clickable(onClick = onClick)
+//                .clip(RoundedCornerShape(15.dp))
+//                .padding(20.dp, 10.dp, 20.dp, 5.dp)
+//                .fillMaxWidth()
+//        ) {
+//            Column (modifier = Modifier
+//                .padding(32.dp)
+//                .fillMaxWidth(),
+//                verticalArrangement = Arrangement.Top,
+//                horizontalAlignment = Alignment.CenterHorizontally){
+//
+//                Text("파이 차트 시각영역")
+//            }
+//        }
+//
+//        Column (
+//            modifier = Modifier
+//                .padding(20.dp, 20.dp, 5.dp, 5.dp)
+//                .fillMaxWidth(),
+//            verticalArrangement = Arrangement.Top
+//        ) {
+//            Text("주요 자산 목록 / 최근 활동",
+//                fontSize = 20.sp)
+//        }
+//
+//        val assetList = listOf("은행 잔고 1", "은행 잔고 2", "은행 잔고 3")
+//        LazyColumn (
+//            modifier = Modifier
+//                .fillMaxSize()
+//        ) {
+//            items(assetList) { asset ->
+//                Text(text = asset, modifier = Modifier.padding(16.dp))
+//                HorizontalDivider()
+//            }
+//        }
+//    }
 
 @Preview(showBackground = true)
 @Composable
